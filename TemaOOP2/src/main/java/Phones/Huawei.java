6fc0e9e1497c8584f0e2397e1250b7b836371c58
AdrianCuciureanu <adrian.cuciureanu@yahoo.com>
2021-06-18 16:09:02 +0300

    versiunea 2 la tema

--- TemaOOP2/src/main/java/Phones/Huawei.java
+++
@@ -1,9 +0,0 @@
-package Phones;
-
-abstract class Huawei implements Phone {
-    private final String manufacturer = "Huawei";
-
-    public String getManufacturer() {
-        return manufacturer;
-    }
-}
